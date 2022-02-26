690a9583733514449a9705e7149aebde54b0d191
viyo <viyo@itu.dk>
2022-02-26 08:49:37 +0100

    added few packages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-//we can set this up to automatically parse a json to an object
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
